3e69018e29d1241b1aa252cd3750c9c5e8066d48
rimaalali <merela349@gmail.com>
2024-11-16 22:38:08 +0300

    Initial commit

---
+++ src/componante/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './footer.css'
+
+export default function Footer() {
+  return (
+    <div className='one'>
+      <p> Copyright © 2024 Villa Focal-X Agency., Ltd. All
+         rights reserved. Design: 
+         <a  className="link" rel="nofollow" hreftarget="_blank">Mohammed Alsaleh for Focal-X</a>  </p>
+         
+    </div>
+  )
+}
